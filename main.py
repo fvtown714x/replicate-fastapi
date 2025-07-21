721be89ac0df4d8eb2263053f7f6715d52fa4373
Diego Cesar <diegooczar@gmail.com>
2025-07-21 09:50:11 -0300

    .

--- main.py
+++ main.py
@@ -21,8 +21,8 @@ API_BASE_URL = "https://replicate-fastapi.onrender.com"
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://jobodega.webflow.io",
-        "https://www.jobodega.com"
+        "https://jobodega.webflow.io/replicate",
+        "https://www.jobodega.com/replicate"
     ],
     allow_credentials=True,
     allow_methods=["*"],
